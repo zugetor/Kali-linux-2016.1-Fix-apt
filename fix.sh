2a310b262db928e50d2e9bdc93ee47fe3cd5557c
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2016-09-11 07:27:32 +0000

    Fix missing link

--- fix.sh
+++ fix.sh
@@ -2,17 +2,17 @@
 apt-get -y remove apt --allow-remove-essential
 check=`uname -m`
 if [ ${check} == 'x86_64' ]; then
-	wget -O libapt.deb "http://http.kali.org/pool/main/a/apt/libapt-pkg5.0_1.3~pre3_amd64.deb"
+	wget -O libapt.deb "http://http.kali.org/pool/main/a/apt/libapt-pkg5.0_1.3~rc4_amd64.deb"
 	dpkg -i libapt.deb
-	wget -O apt.deb "http://http.kali.org/pool/main/a/apt/apt_1.3~pre3_amd64.deb"
+	wget -O apt.deb "http://http.kali.org/pool/main/a/apt/apt_1.3~rc4_amd64.deb"
 	dpkg -i apt.deb
 	wait
 	apt-get clean
 	apt-get update
 else
-  	wget -O libapt.deb "http://http.kali.org/pool/main/a/apt/libapt-pkg5.0_1.3~pre3_i386.deb"
+  	wget -O libapt.deb "http://http.kali.org/pool/main/a/apt/libapt-pkg5.0_1.3~rc4_i386.deb"
 	dpkg -i libapt.deb
-	wget -O apt.deb "http://http.kali.org/pool/main/a/apt/apt_1.3~pre3_i386.deb"
+	wget -O apt.deb "http://http.kali.org/pool/main/a/apt/apt_1.3~rc4_i386deb"
 	dpkg -i apt.deb
 	wait
 	apt-get clean
